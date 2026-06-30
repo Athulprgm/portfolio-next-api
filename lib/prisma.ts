cbc3c3bd67cbf96b4a4e1006faaa698b15d23f1f
Athul Krishna <athulkrishna.cpd@gmail.com>
2026-06-30 15:17:44 +0530

    Fix PrismaClient build-time error

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,15 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient();
+export const prisma = globalForPrisma.prisma || new Proxy({} as PrismaClient, {
+  get: (target, prop) => {
+    if (!globalForPrisma.prisma) {
+      globalForPrisma.prisma = new PrismaClient();
+    }
+    return (globalForPrisma.prisma as any)[prop];
+  }
+});
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
